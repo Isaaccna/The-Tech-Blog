a162ed412c0b7369711a87c574827522e2cb4234
Isaaccna <isaac.andrade1231@gmail.com>
2022-01-31 17:41:01 -0500

    handlebars , public/javascript, routes added

---
+++ public/javascript/add-post.js
@@ -0,0 +1,38 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('input[name="content"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  
+    const newPostButton = document.querySelector('#newpost');
+    const form = document.querySelector('.add-form');
+
+    newPostButton.onclick = function(){
+        
+        form.style.display='block';
+    }
+
+    
+    
+     
+ 
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
