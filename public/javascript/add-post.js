d84ad7f5d4a4540c00388e427e0fc172c6088573
Isaaccna <isaac.andrade1231@gmail.com>
2022-01-31 20:54:20 -0500

    css, last bugs done

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,13 +2,13 @@ async function newFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
-    const content = document.querySelector('input[name="content"]').value;
+    const content = document.querySelector('textarea[name="content"]').value;
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({
         title,
-        content
+        content,
       }),
       headers: {
         'Content-Type': 'application/json'
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -22,4 +22,48 @@ async function loginFormHandler(event) {
     }
   }
   
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+  
+    if (username && email && password) {
+        const response = await fetch('/api/users', {
+          method: 'post',
+          body: JSON.stringify({
+            username,
+            email,
+            password
+          }),
+          headers: { 'Content-Type': 'application/json' }
+        });
+        // check the response status
+if (response.ok) {
+  document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+}
+}
+const signupButton = document.querySelector('#signup');
+    const loginForm = document.querySelector('#logindiv');
+    const signupForm = document.querySelector('#signupdiv');
+    const loginButton = document.querySelector('#login')
+
+    signupButton.onclick = function(){
+     signupForm.style.display = "block";
+     loginForm.style.display = "none";
+    }
+
+    loginButton.onclick = function(){
+      signupForm.style.display = "none";
+      loginForm.style.display = "display";
+     }
+
+    
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+  
+document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
